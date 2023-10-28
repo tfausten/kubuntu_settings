6f321618811d36679435d41af8a775086b95e2ce
Tobias Fausten <tobias.fausten@gmail.com>
2023-10-28 09:42:42 +0900

    add shift-space --> backspace

--- roles/xkeysnail/files/config.py
+++ roles/xkeysnail/files/config.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from xkeysnail.transform import (
     Key,
     K,
@@ -7,15 +5,18 @@ from xkeysnail.transform import (
     define_modmap,
 )
 
-define_keymap(None, {
-    K('Ctrl-h'): Key.LEFT,
-    K('Ctrl-j'): Key.DOWN,
-    K('Ctrl-k'): Key.UP,
-    K('Ctrl-l'): Key.RIGHT,
-    # K('Ctrl-i'): Key.HOME,
-    # K('Ctrl-o'): Key.END,
-}, 'Vim-like cursor')
+define_keymap(
+    None,
+    {
+        K("Ctrl-h"): Key.LEFT,
+        K("Ctrl-j"): Key.DOWN,
+        K("Ctrl-k"): Key.UP,
+        K("Ctrl-l"): Key.RIGHT,
+        K("Shift-Space"): Key.BACKSPACE,
+        # K('Ctrl-i'): Key.HOME,
+        # K('Ctrl-o'): Key.END,
+    },
+    "Vim-like cursor",
+)
 
-define_modmap({
-    Key.CAPSLOCK: Key.LEFT_CTRL
-})
+define_modmap({Key.CAPSLOCK: Key.LEFT_CTRL})
